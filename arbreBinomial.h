bc1d35ab424b83b378a4ae1541aac049f3f5fb1a
3407488 <guil.rako@hotmail.fr>
2018-10-20 16:20:46 +0200

    Ajout de la gestion des arbres binomiaux (il manque surement quelques fonctions

---
+++ arbreBinomial.h
@@ -0,0 +1,57 @@
+#ifndef ARBRE_BINOMIAL_H
+#define ARBRE_BINOMIAL_H
+
+#include "util.h"
+
+// Arbre binomial.
+typedef struct {
+
+	int rank;
+
+	arbreBinomial* parent;
+	listArbreBinomial* listChild;
+
+	bigInt* data;
+
+} arbreBinomial;
+
+// Liste doublement chaine d'abre binomiaux
+// Utile pour les fils des arbres.
+typedef struct {
+
+	listArbreBinomial* previous;
+	listArbreBinomial* next;
+
+	arbreBinomial* data;
+
+} listArbreBinomial;
+
+/*
+Créé un arbre B0 avec une racine ayant pour data la data entree en parametres.
+*/
+arbreBinomial* createB0(bigInt* data);
+
+/*
+Fusione 2 arbres binomiaux entre eux.
+
+Les deux arbres doivent avoir le même rang sinon renvoie NULL.
+
+Renvoie le pointeur sur la racine du nouvel arbre.
+*/
+arbreBinomial* merge(arbreBinomial* a0, arbreBinomial* a1);
+
+/*
+Crée une list d'arbre binomial en initialisant le premier element avec la data entree en parametres.
+*/
+listArbreBinomial* createListArbreBinomial(arbreBinomial* data);
+
+/*
+Ajoute l'element devant la liste.
+Si la liste pointe au niveau du milieu d'une liste, l'ajout se fait proprement et la liste reste integre avec le nouvel element.
+
+Renvoie le pointeur sur le bloque contenant le nouvel element.
+*/
+listArbreBinomial* addElementToListBegin(listArbreBinomial* list, arbreBinomial* element);
+
+#endif // !ARBRE_BINOMIAL_H
+
